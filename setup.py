d985eb5d6f37ea96c62dfbf8fa9ba107e7a58252
Jonathan Yong <yongrenjie@gmail.com>
2021-05-21 18:19:18 +0100

    v0.2: enable caching by default

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sphinx-exec-directive',
-    version='0.1',
+    version='0.2',
     description='Run Python code blocks and display the output directly within Sphinx documentation',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- sphinx_exec_directive/__init__.py
+++ sphinx_exec_directive/__init__.py
@@ -1,8 +1,11 @@
+import os
 import io
+from hashlib import md5
 from contextlib import redirect_stdout
+from pathlib import Path
 
 from docutils import nodes
-from docutils.parsers.rst import Directive
+from docutils.parsers.rst import directives, Directive
 
 
 context = dict()
@@ -36,16 +39,56 @@ def _option_boolean(arg):
 class Exec(Directive):
     has_content = True
     required_arguments = 0
-    optional_arguments = 1
+    optional_arguments = 2
     option_spec = {
         'context': _option_boolean,
+        'cache': _option_boolean,
     }
 
     def run(self):
         save_context = self.options.get('context', False)
+        # Don't cache if the user requests saving context. The reason is
+        # because the global_dict can't be updated just by reading in code from
+        # a file (as opposed to executing it). I can't be bothered to fix this.
+        cache = not save_context and self.options.get('cache', True)
 
         code_in = "\n".join(self.content)
-        code_out = execute_code(code_in, context)
+
+        if cache:
+            # Get some important paths
+            top_level_sphinx_dir = Path(setup.confdir)
+            # ^ ~/dphil/nbsphinx
+            source_f = Path(self.state_machine.document.attributes['source'])
+            # ^ ~/dphil/nbsphinx/hmbc/yymmdd.rst
+            # Figure out where to dump the output.
+            source_rel_f = source_f.relative_to(top_level_sphinx_dir)
+            # ^ hmbc/yymmdd.rst
+            source_identifier = str(source_rel_f)[:-4].replace('/', '-')
+            # ^ hmbc-yymmdd
+            build_dir = Path(setup.app.doctreedir).parent
+            # ^ ~/dphil/nbsphinx/dirhtml (or whatever)
+            md5_hash = md5(code_in.encode('utf-8')).hexdigest()
+            # ^ HASH
+            output_f = (build_dir
+                         / "exec_directive"
+                         / f"{source_identifier}-{md5_hash}")
+            # ^ ~/dphil/nbsphinx/dirhtml/exec_directive/hmbc-yymmdd-HASH
+
+            cache_found = (output_f.exists()
+                           and
+                           source_f.stat().st_mtime < output_f.stat().st_mtime)
+
+            if cache_found:
+                with open(output_f, "r") as out_f:
+                    code_out = out_f.read()
+            else:
+                code_out = execute_code(code_in, context)
+                if not output_f.parent.exists():
+                    output_f.parent.mkdir()
+                with open(output_f, "w") as out_f:
+                    print(code_out, file=out_f, end="")
+        else:  # cache not enabled
+            code_out = execute_code(code_in, context)
 
         # Reset the context if it's not meant to be preserved
         if not save_context:
@@ -63,10 +106,12 @@ class Exec(Directive):
 
 
 def setup(app):
+    setup.app = app
+    setup.confdir = app.confdir
     app.add_directive("exec", Exec)
 
     return {
-        'version': '0.1',
+        'version': '0.2',
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
