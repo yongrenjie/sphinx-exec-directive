f58c40a3f2abd551795e6142feee742ca1470a84
doyougnu <jeffrey.young@iohk.io>
2023-01-11 07:28:15 -0500

    Add ability to run cabal or stack haskell projects

--- sphinx_exec_directive/__init__.py
+++ sphinx_exec_directive/__init__.py
@@ -33,12 +33,12 @@ class cd:
 
 def execute_code(runner, globals_dict=None):
 
-    def execute_code_with_pipe(command, post_process=[]):
+    def execute_code_with_pipe(command, code_in, post_process=[]):
         proc = subprocess.Popen(command,
                                 stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-        out, err = proc.communicate(input=code.encode("utf-8"))
+        out, err = proc.communicate(input=code_in.encode("utf-8"))
 
         # apply all post processing functions now that we have output
         out = out.decode('utf-8')
@@ -58,20 +58,40 @@ def execute_code(runner, globals_dict=None):
 
         output_object = io.StringIO()
         with redirect_stdout(output_object):
-            exec(code, globals_dict)
+            exec(runner['code_in'], globals_dict)
         code_out = output_object.getvalue()
 
     elif runner['process'] == 'haskell':
         post_process = []
         payload = []
 
-        if  runner['with'] == 'ghci':
+        # check that the runner with field is set
+        # and set post-process hooks
+        if not runner['with']:
+            runner['with'] = 'runghc' # default is runghc, no hooks
+
+        if runner['with'] == 'ghci':
+            # if running with ghci then we post process the output to remove
+            # ghci specific text
             post_process += [lambda s: s.replace("ghci>",""),
                              lambda s: re.sub("^.*?\n", "", s),
                              lambda s: s.replace("Leaving GHCi.\n", "").rstrip()
                             ]
 
-        code_out = execute_code_with_pipe()
+        # do the business
+        if runner['with'] == 'cabal' or runner['with'] == 'stack':
+            if runner['project_dir']:
+                with cd(Path(runner['project_dir'])):
+                    payload   = [runner['with']] + runner['args']
+                    comp_proc = subprocess.run(payload, capture_output=True, text=True)
+                    out       = comp_proc.stdout
+                    err       = comp_proc.stderr
+                # Log
+                if err is not None and err.strip() != "":
+                    print(err) # should use sphinx logger
+                code_out = out
+        else:
+            code_out = execute_code_with_pipe(runner['with'], runner['code_in'], post_process)
 
     elif runner['process'] == 'matlab':
         # MATLAB can't pipe, so we need to dump to a tempfile.
@@ -83,7 +103,7 @@ def execute_code(runner, globals_dict=None):
             with cd(filepath.parent):
                 comp_proc = subprocess.run(['matlab', '-batch', filepath.stem],
                                            capture_output=True, text=True)
-                out = comp_proc.stdout
+                out = comp_proc.stdout.decode('utf-8')
                 err = comp_proc.stderr
         # Log any stderr.
         if err is not None and err.strip() != "":
@@ -131,8 +151,8 @@ class Exec(Directive):
         'cache':     _option_boolean,
         'process':   _option_process,
         'intertext': _option_str,
+        'project_dir': _option_str,
         'with':      _option_str,
-        'target':    _option_str,
         'args':      _option_str
     }
 
@@ -155,20 +175,22 @@ class Exec(Directive):
                  and len(context) == 0
                  and self.options.get('cache', True))
         process = self.options.get('process', 'python')
-
+        project_dir = self.options.get('project_dir', '')
         opt_with = self.options.get('with', '')
-        target   = self.options.get('target','')
-        args     = self.options.get ('args','')
+        args     = self.options.get ('args','').split()
 
         # A runner is "that which runs the code", i.e., a dictionary that
         # defines the entire external process
         runner = {'process': process,  # the language
                   'with':    opt_with, # to run with what tool/binary
-                  'target':  target,   # which target to run in the case we are
-                                       # not running a literate code block
-                  'code_in': '',       # The code to run in case we are running
-                                       # a literate code block
-                  'args':    args}     # extra args passed to with
+                  'project_dir': project_dir,   # if we're running from project
+                                                # then store the project dir
+                                                # 'code_in': '', # The code to
+                                                # run, if from file then this is
+                                                # the contents of source_file,
+                                                # if not then its the contents
+                                                # of a literal code block
+                  'args':    args}     # args to run with, with
 
         # Determine whether input is to be read from a file, or directly from
         # the exec block's contents.
@@ -182,18 +204,20 @@ class Exec(Directive):
         #                        _pRF is a  relative path to a file.
         top_level_sphinx_pAD = Path(setup.confdir)
 
-        # Determine where to get source code from.
+        # Determine where to get source code from. If we are using a build
+        # system then the user file is actually a project directory
         if from_file:
             # Set the 'source file' to be the specified file. The argument to
             # the exec block is given as a relative path, so has to be made
             # absolute with respect to the top-level Sphinx directory.
             source_pAF = top_level_sphinx_pAD.joinpath(Path(self.arguments[0]))
-            code_in = source_pAF.read_text()
+            runner['code_in'] = source_pAF.read_text()
+            runner['source_file'] = source_pAF
         else:
             # Set the 'source file' to be the rst file which the code is in.
             # This path is already absolute.
             source_pAF = current_rst
-            code_in = "\n".join(self.content)
+            runner['code_in'] = "\n".join(self.content)
 
         # Look up the output in the cache, or execute the code.
         if cache:
@@ -208,7 +232,7 @@ class Exec(Directive):
             else:
                 source_identifier = source_pRF.with_suffix('')
                 source_identifier = str(source_identifier).replace('/', '-')
-                md5_hash = md5(code_in.encode('utf-8')).hexdigest()
+                md5_hash = md5(runner['code_in'].encode('utf-8')).hexdigest()
                 identifier = (f"{source_identifier}-{process}-"
                               f"inline-{md5_hash}.out")
                 # ^ folder-yymmdd-python-inline-<HASH>.out
@@ -224,19 +248,19 @@ class Exec(Directive):
                 with open(output_pAF, "r") as out_f:
                     code_out = out_f.read()
             else:
-                code_out = execute_code(code_in, process, context)
+                code_out = execute_code(runner, context)
                 if not output_pAF.parent.exists():
                     output_pAF.parent.mkdir()
                 with open(output_pAF, "w") as out_f:
                     print(code_out, file=out_f, end="")
         else:  # caching was disabled, execute it
-            code_out = execute_code(code_in, process, context)
+            code_out = execute_code(runner, context)
 
         # Reset the context if it's not meant to be preserved
         if not save_context:
             context.clear()
 
-        node_in = nodes.literal_block(code_in, code_in)
+        node_in = nodes.literal_block(runner['code_in'], runner['code_in'])
         node_out = nodes.literal_block(code_out, code_out)
         node_in['language'] = process
         node_out['language'] = 'none'
