a21b7ae075431340eb0ffdc0e6ca3df649028a0e
Jonathan Yong <yongrenjie@gmail.com>
2023-01-25 18:40:20 +0000

    add envvar to open html file during tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import shutil
 from subprocess import PIPE, Popen
@@ -9,6 +10,14 @@ import pytest
 def test_sphinx_build(tmp_path):
     """
     Ensure that Sphinx build completes without errors or warnings.
+
+    Note that this doesn't check in any way that the output is actually correct
+    in any way. In order to do so, set the environment variable
+
+        SPHINX_EXEC_DIRECTIVE_OPENHTML=1
+
+    This will use either `open` or `xdg-open` to open the built HTML pages so
+    that they can be inspected for correctness.
     """
     source_dir = tmp_path / 'rst_source'
     shutil.copytree(Path(__file__).parent / 'rst_source', source_dir)
@@ -23,5 +32,9 @@ def test_sphinx_build(tmp_path):
         f"sphinx build failed with stdout:\n{out}\nstderr:\n{err}\n"
     if err:
         pytest.fail(f"sphinx build emitted the following warnings:\n{err}")
+
+    # open HTML if requested
+    if os.environ.get("SPHINX_EXEC_DIRECTIVE_OPENHTML", "") == "1":
+        Popen(["open", f"{str(build_dir)}/index.html"])
 
     assert build_dir.is_dir()
